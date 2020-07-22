ef00423e411d6710c49d531686cc7ada2ef92198
= <shakirck333@gmail.com>
2020-07-22 13:08:05 +0530

    dd store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './components/App';
+import { configureStore } from './store';
 
+const store = configureStore();
+console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ src/reducers/posts.js
@@ -0,0 +1,3 @@
+export default function posts(state = ['dfsd', 'dfad']) {
+  return state;
+}
