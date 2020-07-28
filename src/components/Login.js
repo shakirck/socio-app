1e44c37be6e8475d7ac307a821c20b93fff951d5
= <shakirck333@gmail.com>
2020-07-28 15:53:29 +0530

    initial setup for user login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,13 +3,29 @@ import React, { Component } from 'react';
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.emailInputRef = React.createRef();
-    this.passwordInputRef = React.createRef();
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
   handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(this.emailInputRef, this.passwordInputRef);
+    console.log(this.state);
   };
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
   render() {
     return (
       <form className="login-form">
@@ -18,13 +34,15 @@ export default class Login extends Component {
           <input
             type="email"
             placeholder="Email"
-            ref={this.emailInputRef}
+            // ref={this.emailInputRef}
+            onChange={this.handleEmailChange}
             required
           />
           <input
             type="password"
             placeholder="Password"
-            ref={this.passwordInputRef}
+            // ref={this.passwordInputRef}
+            onChange={this.handlePasswordChange}
             required
           />
         </div>
---
+++ src/reducers/auth.js
@@ -0,0 +1,33 @@
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/actionTypes';
+
+const initialAuthState = {
+  user: {},
+  error: null,
+  isLoggedIn: false,
+  inProgress: false,
+};
+export default function (initialAuthState, action) {
+  switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        inProgress: true,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+
+        user: action.user,
+        isLoggedIn: true,
+        error: null,
+      };
+    case LOGIN_FAIL:
+      return {
+        ...state,
+        error: action.error,
+        inProgress: false,
+      };
+    default:
+      return state;
+  }
+}
