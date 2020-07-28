f7a69b0e6e3a869c7dfe4204d5a07e6b715ea667
= <shakirck333@gmail.com>
2020-07-28 13:29:07 +0530

    add uncontrolled referecnce for logi

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.emailInputRef = React.createRef();
+    this.passwordInputRef = React.createRef();
+  }
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.emailInputRef, this.passwordInputRef);
+  };
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header"> Login</span>
         <div className="field">
-          <input type="email" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
+          <input
+            type="email"
+            placeholder="Email"
+            ref={this.emailInputRef}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            ref={this.passwordInputRef}
+            required
+          />
         </div>
         <div className="field">
-          <button>Login</button>
+          <button onClick={this.handleFormSubmit}>Login</button>
         </div>
       </form>
     );
