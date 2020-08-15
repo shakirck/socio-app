4007ba263df126cfe184f44f3bf215ef2bc14939
= <shakirck333@gmail.com>
2020-08-15 21:21:32 +0530

    Updates

---
+++ precache-manifest.0f191debc3d22590f6fe631837eb73a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec27f4e47fdbffe622f7b60827bc807",
+    "url": "/socio/index.html"
+  },
+  {
+    "revision": "79f673dd7c1eb926a41e",
+    "url": "/socio/static/css/main.16dabb2a.chunk.css"
+  },
+  {
+    "revision": "64a90b50535bb4680874",
+    "url": "/socio/static/js/2.2e62c6b7.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/socio/static/js/2.2e62c6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f673dd7c1eb926a41e",
+    "url": "/socio/static/js/main.77c6aa0e.chunk.js"
+  },
+  {
+    "revision": "67f37987358fe61e6262",
+    "url": "/socio/static/js/runtime-main.b12e7f73.js"
+  }
+]);
